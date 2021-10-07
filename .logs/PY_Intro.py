6b879c387db57baadf725542a1597e66cddd2070
Zu Rong Hun <00ZRHun@gmail.com>
2021-10-07 13:18:53 +0000

    original folder

---
+++ .logs/PY_Intro.py
@@ -0,0 +1,38 @@
+%logstop
+%logstart -rtq ~/.logs/PY_Intro.py append
+import seaborn as sns
+sns.set()
+# Thu, 07 Oct 2021 13:01:49
+%logstop
+%logstart -rtq ~/.logs/PY_Intro.py append
+import seaborn as sns
+sns.set()%logstop
+%logstart -rtq ~/.logs/PY_Intro.py append
+import seaborn as sns
+sns.set()
+%logstop
+%logstart -rtq ~/.logs/PY_Intro.py append
+import seaborn as sns
+sns.set()
+# Thu, 07 Oct 2021 13:01:56
+%logstop
+%logstart -rtq ~/.logs/PY_Intro.py append
+import seaborn as sns
+sns.set()%logstop
+%logstart -rtq ~/.logs/PY_Intro.py append
+import seaborn as sns
+sns.set()
+%logstop
+%logstart -rtq ~/.logs/PY_Intro.py append
+import seaborn as sns
+sns.set()
+%logstop
+%logstart -rtq ~/.logs/PY_Intro.py append
+import seaborn as sns
+sns.set()
+# Thu, 07 Oct 2021 13:01:59
+print('some of which are code')# Thu, 07 Oct 2021 13:02:53
+a = 5# Thu, 07 Oct 2021 13:02:58
+print(a)# Thu, 07 Oct 2021 13:03:59
+print(a)
+!python --version
---
+++ .logs/global_log.py
@@ -0,0 +1,5 @@
+# Thu, 07 Oct 2021 12:58:05
+%logstop
+%logstart -rtq ~/.logs/PY_Intro.py append
+import seaborn as sns
+sns.set()
